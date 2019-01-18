792126cae2a548149f0b3567b7e09a3f4092460c
Rangaswini Sanjay Khandare <rangaswini.khandare@qed42.com>
2019-01-18 16:01:08 +0530

    PHP forms-completed

--- index.php
+++ index.php
@@ -1,152 +1,116 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>  
 <html>
-<body>
-
-<h1>My first PHP page</h1>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
 
 <?php
-// 1.  
-echo "Hello World!";
-// 2. 
-$color = "red";
-echo "My car is " . $color . "<br>";
-//3
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
-echo $txt.$x.$y;
-// 4.
-print $x + $y;
-// 5.
-$x = 5985;
-var_dump($x);
-// 6.
-echo strlen("Hello world!");
-// 7.
-echo str_word_count("Hello world!");
-// 8
-echo strrev("Hello world!");
-//9
-echo strpos("Hello world!", "l"); //o/p=>2
-//10
-echo str_replace("world", "Rango", "Hello world!");
-//11
-define("GREETING", "Gm");
-echo GREETING;
-//12 Concatenation assignment
-$t1="hi";
-$t2="rang";
-$t1 .= $t2;
-echo $t1;
-//13 
-$t = 2;
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-if ($t < 5) {
-    echo "Have a good day!";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } 
+  elseif($_POST["name"]) {
+   $name = test_input($_POST["name"]);
+   // check if name only contains letters and whitespace
+   if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+     $nameErr = "Only letters and white space allowed"; 
+   }
 }
-//14 switch
-$favcolor = "black";
-
-switch ($favcolor) {
-    case "red":
-        echo "Your favorite color is red!";
-        break;
-    case "blue":
-        echo "Your favorite color is blue!";
-        break;
-    case "green":
-        echo "Your favorite color is green!";
-        break;
-    default:
-        echo "Your favorite color is neither red, blue, nor green!";
+  else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } 
+  elseif($_POST["email"])
+  {
+      $email = test_input($_POST["email"]);
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; }
 }
-//15
-$x = 0; 
 
-while($x <= 2) {
-    echo "The number is: $x <br>";
-    $x++;
-} 
-//16
-$xx = 6;
-
-do {
-    echo "The number is: $xx <br>";
+else {
+    $email = test_input($_POST["email"]);
+  }
     
-} while ($xx <= 5);
+  if (empty($_POST["website"])) {
+    $website = "";
+  } 
+  elseif($_POST["website"])
+{
+$website = test_input($_POST["website"]);
+if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+  $websiteErr = "Invalid URL"; 
+}
+}
+else {
+    $website = test_input($_POST["website"]);
+  }
 
-//17
-for ($x = 0; $x <= 10; $x++) {
-   echo "The number is: $x <br>";
-} 
-//18 foreach
-$colors = array("red", "green", "blue", "yellow"); 
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
 
-foreach ($colors as $value) {
-    echo "$value <br>";
-}
-//19function
-function msg() {
-   echo "Rangaswini";
-}
-msg(); 
-//20
-function msgName($name, $nickname="no nick name") {
-   echo "$name => $nickname <br>";
-}
-msgName("Rangaswini", "Rango");
-function returnFun($a,$b=0) {
-   return $a+$b;
-}
-echo"function_returning_addition=>".returnFun(3,4);
-//21 Array
-//Index array
-$color = array("pink", "blue", "white");
-echo $color[0];
-echo count($color);//length
-//Associative array
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-echo"Age of Ben =>".$age['Ben'];
-
-foreach($age as $x => $x_value) {
-   echo "Key=" . $x . ", Value=" . $x_value;
-   echo "<br>";
-}
-sort($color);
-for($x = 0; $x < count($color); $x++) {
-   echo $color[$x];
-}
-rsort($color);
-asort($age);
-ksort($age);
-arsort($age) ;
-krsort($age);
-
-//22 Global and Superglobal
-$var1=2;
-$var2=3;
-function addition() { 
-   $GLOBALS['z'] = $GLOBALS['var1'] + $GLOBALS['var2']; 
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
 }
 
-addition(); 
-echo $z; 
-//
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
 
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
 </body>
 </html>
